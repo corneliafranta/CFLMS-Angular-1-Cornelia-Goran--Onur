4ef330f0752ff868732518f014e2288cb7de4f0a
Goran Ivkić <65123282+gogifswd10@users.noreply.github.com>
2020-06-22 21:34:58 +0200

    angular day prework 01 ex.done

---
+++ Goran/myAngularApp/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {Navbar} from "../navbar"
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'myAngularApp';
+}
